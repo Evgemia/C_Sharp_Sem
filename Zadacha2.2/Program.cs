20c8035d4b3c235c52acf821d1a22332fe2da985
Ваше имя английскими буквами <Zhenka_512@mail.ru>
2023-02-17 22:42:54 +0300

    выводит случайное число и удаляет вторую  цифру числа

---
+++ Zadacha2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу,которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//456->46
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a3 = num % 10;
+Console.WriteLine($"{num} -> {a1} {a3}");// если с этим числом больше не нужно работать
+
+/*Console.Clear();
+int num = new Random().Next(100, 1000);
+int result = (num/100) *10 + num %10;
+Console.WriteLine($"{num} -> {result}");
+Console.WriteLine($"{num} -> {num/100}{num%10}");*/
