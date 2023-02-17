996fd742bc90684063574df25dd869d45751670a
Ваше имя английскими буквами <Zhenka_512@mail.ru>
2023-02-17 23:49:03 +0300

    кратно ли число одновременно 7 и 23

---
+++ Zadacha2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number =int.Parse(Console.ReadLine());
+
+if (number % 23 == 0 && number % 7 == 0)
+{
+    Console.WriteLine("кратно");
+    
+}
+else
+{
+    Console.WriteLine("не кратно");
+}
