704513f37165b17ccc5bdd512954f4c87b35a71d
Ваше имя английскими буквами <Zhenka_512@mail.ru>
2023-02-17 21:59:39 +0300

    выводит случайное число и показывает макс цифру числа

--- Zadacha2.1/Program.cs
+++ Zadacha2.1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу,котоая выводит случайное число  из отрезка [10, 99] и показывает наибольшую цифру*/
+
+Console.Clear();
+int num = new Random().Next(10, 100);
+int a1 = num / 10;
+int a2 = num % 10;
+int max = a1;
+if (max < a1)
+    max = a1;
+Console.WriteLine($"Максимальная цифра число {num} -> {max}");
